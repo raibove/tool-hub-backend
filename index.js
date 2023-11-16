61f15495d951ad13a30bf890dfcc0f52251f8c01
Shweta <shwetakale144@gmail.com>
2023-11-16 10:51:44 +0530

    integrate pangea domain intel

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express');
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
-const cors = require('cors');
+import {
+    PangeaConfig,
+    DomainIntelService,
+    PangeaErrors,
+} from "pangea-node-sdk";
+import express from 'express';
+import cors from 'cors';
+import { createClient } from "@supabase/supabase-js";
+import { config as envConfig } from "dotenv";
+envConfig();
+
 // Initialize Supabase client with your Supabase project URL and public API key
 const supabase = createClient(process.env.SUPABASE_URL || "", process.env.SUPABASE_KEY || "");
 
@@ -11,32 +18,62 @@ const port = 5000; // You can change the port as needed
 app.use(cors());
 app.use(express.json());
 
+
+const domain = process.env.PANGEA_DOMAIN;
+const token = process.env.PANGEA_CLIENT_TOKEN;
+const config = new PangeaConfig({ domain: domain });
+const domainIntel = new DomainIntelService(String(token), config);
 // Route to handle product submissions
 app.post('/submit-tool', async (req, res) => {
-  const { productUrl, description, mail } = req.body;
+    const { productUrl, description, mail } = req.body;
 
-  // Validate the URL (you can use the isValidUrl function from the previous example)
+    const urlObject = new URL(productUrl);
+    const domainUrl = urlObject.hostname;
 
-  // Insert data into the 'products' table in Supabase
-  const { data, error } = await supabase
-    .from('products')
-    .upsert([
-      {
-        product_url: productUrl,
-        product_description: description,
-        user_mail: mail
-      },
-    ]);
+    try {
+        const response = await domainIntel.reputation(domainUrl, { provider: "domaintools" });
+        if (response.status === "Success" &&
+            (response.result.data.verdict === "unknown" || response.result.data.verdict === "benign")) {
+            const supabaseResponse = await saveToSupabase(productUrl, description, mail);
+            if (supabaseResponse.error) {
+                console.error('Error saving data to Supabase:', supabaseResponse.error);
+                res.status(500).json({ error: 'Error in saving tool' });
+            } else {
+                console.log('Data saved to Supabase successfully:', supabaseResponse.data);
+                res.json({ success: 'Tool submitted successfully' });
+            }
+        } else {
+            console.log('Malicious domain detected. Data not saved.');
+            res.status(403).json({ error: 'Malicious domain detected. Data not saved.' });
+        }
+    } catch (e) {
+        console.error('Error checking domain reputation:', e.message);
+        res.status(500).json({ error: 'Error checking domain reputation' });
+    }
 
-  if (error) {
-    console.error('Error inserting data:', error);
-    return res.status(500).json({ error: 'Error inserting data' });
-  } else {
-    console.log('Data inserted successfully:', data);
-    return res.json({ success: 'Data inserted successfully' });
-  }
 });
 
+const saveToSupabase = async (productUrl, description, mail) => {
+    try {
+        const { data, error } = await supabase
+            .from('products')
+            .upsert([
+                {
+                    product_url: productUrl,
+                    product_description: description,
+                    user_mail: mail
+                },
+            ]);
+        if (error) {
+            return { error: 'Error inserting data into Supabase' };
+        } else {
+            return { data: 'data inserted successfully' };
+        }
+    } catch (error) {
+        return { error: 'Error inserting data into Supabase' };
+    }
+}
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
